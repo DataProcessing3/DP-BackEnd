011237385013f85cce7e37c75f42ace2f77223ca
koreaioi <koreaioi@gachon.ac.kr>
2024-10-11 01:42:34 +0900

    초기 세팅

---
+++ src/main/java/com/DataProcessing3/DP_BackEnd/DpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.DataProcessing3.DP_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DataProcessing3/DP_BackEnd/DpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataProcessing3.DP_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
